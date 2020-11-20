2699bb15d4b9dfee0c7497744a8ee9aa6154ddd5
Raynon Messias <raymessias@gmail.com>
2020-11-20 16:05:13 -0300

    Commit: Auth with jwt

--- projectNode/src/services/AuthenticateUserService.ts
+++ projectNode/src/services/AuthenticateUserService.ts
@@ -1,5 +1,6 @@
 import { getRepository } from 'typeorm'
 import { compare } from 'bcryptjs'
+import { sign } from 'jsonwebtoken'
 
 import User from '../models/User'
 
@@ -10,6 +11,7 @@ interface Request {
 
 interface Response {
   user: User
+  token: string
 }
 
 class AuthenticateUSerService {
@@ -28,8 +30,14 @@ class AuthenticateUSerService {
       throw new Error('Incorrect email/password combination.')
     }
 
+    const token = sign({}, '32d6abcad6d97ac2507b3153e592e649', {
+      subject: user.id,
+      expiresIn: '1d',
+    })
+
     return {
       user,
+      token,
     }
   }
 }
