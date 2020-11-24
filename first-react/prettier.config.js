9f0b038608eb5d75ec55a73b72dfe46460b2e817
Raynon Messias <raymessias@gmail.com>
2020-11-24 20:38:18 -0300

    Github Explorer react aplication

--- first-react/prettier.config.js
+++ first-react/prettier.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   singleQuote: true,
   trailingComa: 'all',
-  allowParens: 'avoid',
+  arrowParens: 'avoid',
   semi: false,
 }
--- first-react/src/pages/Dashboard/index.tsx
+++ first-react/src/pages/Dashboard/index.tsx
@@ -1,10 +1,11 @@
-import React, { useState, FormEvent } from 'react'
+import React, { useState, useEffect, FormEvent } from 'react'
 import { FiChevronRight } from 'react-icons/fi'
+import { Link } from 'react-router-dom'
 import api from '../../services/api'
 
 import LogoImg from '../../assets/github-logo.svg'
 
-import { Title, Form, Repositories } from './styles'
+import { Title, Form, Repositories, Error } from './styles'
 
 interface Repository {
   full_name: string
@@ -17,18 +18,45 @@ interface Repository {
 
 const Dashboard: React.FC = () => {
   const [newRepo, setNewRepo] = useState('')
-  const [repositories, setRepositories] = useState<Repository[]>([])
+  const [inputError, setInputError] = useState('')
+  const [repositories, setRepositories] = useState<Repository[]>(() => {
+    const storagedRepositories = localStorage.getItem(
+      '@GithubExplorer:repositories'
+    )
+
+    if (storagedRepositories) {
+      return JSON.parse(storagedRepositories)
+    }
+    return []
+  })
+
+  useEffect(() => {
+    localStorage.setItem(
+      '@GithubExplorer:repositories',
+      JSON.stringify(repositories)
+    )
+  }, [repositories])
 
   async function handleAddRepository(
     event: FormEvent<HTMLFormElement>
   ): Promise<void> {
     event.preventDefault()
 
-    const response = await api.get<Repository>(`repos/${newRepo}`)
+    if (!newRepo) {
+      setInputError('Digite o autor/nome do repositório')
+      return
+    }
+    try {
+      const response = await api.get<Repository>(`repos/${newRepo}`)
 
-    const repository = response.data
+      const repository = response.data
 
-    setRepositories([...repositories, repository])
+      setRepositories([...repositories, repository])
+      setNewRepo('')
+      setInputError('')
+    } catch (err) {
+      setInputError('Erro na busca por esse repositório')
+    }
   }
 
   return (
@@ -36,7 +64,7 @@ const Dashboard: React.FC = () => {
       <img src={LogoImg} alt="Github Explorer" />
       <Title>Explore repositórios no Github</Title>
 
-      <Form onSubmit={handleAddRepository}>
+      <Form hasError={!!inputError} onSubmit={handleAddRepository}>
         <input
           value={newRepo}
           onChange={(e) => setNewRepo(e.target.value)}
@@ -45,19 +73,24 @@ const Dashboard: React.FC = () => {
         <button type="submit">Pesquisar</button>
       </Form>
 
+      {inputError && <Error>{inputError}</Error>}
+
       <Repositories>
         {repositories.map((repository) => (
-          <a key={repository.full_name} href="teste">
+          <Link
+            to={`/repositories/${repository.full_name}`}
+            key={repository.full_name}
+          >
             <img
-              src="https://avatars1.githubusercontent.com/u/59324178?s=460&u=46b74c3fd80171032633db1beca2e3ace2990992&v=4"
-              alt="Raynon Messias"
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
             />
             <div>
-              <strong>raymessias/gostack11</strong>
-              <p>Easy peasy highly scalable ReactJs & react Native forms!</p>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
             </div>
             <FiChevronRight size={20} />
-          </a>
+          </Link>
         ))}
       </Repositories>
     </>
