a5a19eb77f8036c0c07592eb0c66c0ba7a10a7bf
Raynon Messias <raymessias@gmail.com>
2020-11-25 21:54:52 -0300

    Create SignUp page

--- gobarber-web/src/App.tsx
+++ gobarber-web/src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 
 import GlobalStyle from './styles/global'
-import SignIn from './pages/Siginin'
+// import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
 
 const App: React.FC = () => (
   <>
-    <SignIn />
+    <SignUp />
     <GlobalStyle />
   </>
 )
