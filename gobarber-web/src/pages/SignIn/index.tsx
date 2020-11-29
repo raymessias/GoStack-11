72d0093aa7418f668100ea0750fcaf94a74787af
Raynon Messias <raymessias@gmail.com>
2020-11-29 20:53:22 -0300

    Create apiContext Sign

--- gobarber-web/src/pages/SignIn/index.tsx
+++ gobarber-web/src/pages/SignIn/index.tsx
@@ -1,8 +1,10 @@
 import * as Yup from 'yup'
 import { Form } from '@unform/web'
 import { FormHandles } from '@unform/core'
-import React, { useRef, useCallback } from 'react'
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi'
+import React, { useRef, useCallback, useContext } from 'react'
+
+import { AuthContext } from '../../context/AuthContext'
 
 import getValidationErrors from '../../utils/getValidationErrors'
 
@@ -13,29 +15,46 @@ import Button from '../../components/Button'
 
 import { Container, Content, Background } from './styles'
 
+interface SignInFormData {
+  email: string
+  password: string
+}
+
 const SignIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null)
 
+  const { user, signIn } = useContext(AuthContext)
+
+  console.log(user)
+
   // data: object -> porem gera erro
-  const handleSubmit = useCallback(async (data: Record<string, unknown>) => {
-    try {
-      formRef.current?.setErrors({})
+  const handleSubmit = useCallback(
+    async (data: SignInFormData) => {
+      try {
+        formRef.current?.setErrors({})
 
-      const schema = Yup.object().shape({
-        email: Yup.string()
-          .required('E-mail obrigatório')
-          .email('Digite um e-mail válido'),
-        password: Yup.string().required('Senha obrigatória'),
-      })
+        const schema = Yup.object().shape({
+          email: Yup.string()
+            .required('E-mail obrigatório')
+            .email('Digite um e-mail válido'),
+          password: Yup.string().required('Senha obrigatória'),
+        })
 
-      await schema.validate(data, {
-        abortEarly: false,
-      })
-    } catch (err) {
-      const errors = getValidationErrors(err)
-      formRef.current?.setErrors(errors)
-    }
-  }, [])
+        await schema.validate(data, {
+          abortEarly: false,
+        })
+
+        signIn({
+          email: data.email,
+          password: data.password,
+        })
+      } catch (err) {
+        const errors = getValidationErrors(err)
+        formRef.current?.setErrors(errors)
+      }
+    },
+    [signIn]
+  )
   return (
     <Container>
       <Content>
