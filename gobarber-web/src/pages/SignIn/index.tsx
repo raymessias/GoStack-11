49626bae5ce66d2463e8b43997ee70fa44299f35
Raynon Messias <raymessias@gmail.com>
2020-11-30 00:53:03 -0300

    gobarberweb: Create feature called toast messages

--- gobarber-web/src/pages/SignIn/index.tsx
+++ gobarber-web/src/pages/SignIn/index.tsx
@@ -1,10 +1,11 @@
-import * as Yup from 'yup'
-import { Form } from '@unform/web'
-import { FormHandles } from '@unform/core'
+import React, { useRef, useCallback } from 'react'
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi'
-import React, { useRef, useCallback, useContext } from 'react'
+import { FormHandles } from '@unform/core'
+import { Form } from '@unform/web'
+import * as Yup from 'yup'
 
-import { AuthContext } from '../../context/AuthContext'
+import { useAuth } from '../../hooks/auth'
+import { useToast } from '../../hooks/toast'
 
 import getValidationErrors from '../../utils/getValidationErrors'
 
@@ -23,10 +24,8 @@ interface SignInFormData {
 const SignIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null)
 
-  const { user, signIn } = useContext(AuthContext)
-
-  console.log(user)
-
+  const { signIn } = useAuth()
+  const { addToast } = useToast()
   // data: object -> porem gera erro
   const handleSubmit = useCallback(
     async (data: SignInFormData) => {
@@ -44,16 +43,23 @@ const SignIn: React.FC = () => {
           abortEarly: false,
         })
 
-        signIn({
+        await signIn({
           email: data.email,
           password: data.password,
         })
       } catch (err) {
-        const errors = getValidationErrors(err)
-        formRef.current?.setErrors(errors)
+        if (err instanceof Yup.ValidationError) {
+          const errors = getValidationErrors(err)
+          formRef.current?.setErrors(errors)
+        }
+        addToast({
+          type: 'error',
+          title: 'Erro na autenticação',
+          description: 'Ocorreu um erro ao fazer login, cheque as credenciais.',
+        })
       }
     },
-    [signIn]
+    [signIn, addToast]
   )
   return (
     <Container>
