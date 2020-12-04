d99aabb01a873647a139a91b288ad257a2ba6cc7
Raynon Messias <raymessias@gmail.com>
2020-12-03 23:10:01 -0300

    appgobarber: consume api backend to login and logout
    Create feature api and new routes to authenticated users

--- appgobarber/src/pages/SignUp/index.tsx
+++ appgobarber/src/pages/SignUp/index.tsx
@@ -14,6 +14,7 @@ import {
   KeyboardAvoidingView,
 } from 'react-native'
 
+import api from '../../services/api'
 import getValidationErrors from '../../utils/getValidationErrors'
 
 import Input from '../../components/Input'
@@ -30,44 +31,51 @@ interface SignUpFormData {
 
 const SignUp: React.FC = () => {
   const formRef = useRef<FormHandles>(null)
+  const navigation = useNavigation()
 
   const emailInputRef = useRef<TextInput>(null)
   const passwordInputRef = useRef<TextInput>(null)
 
-  const handleSignUp = useCallback(async (data: SignUpFormData) => {
-    try {
-      formRef.current?.setErrors({})
+  const handleSignUp = useCallback(
+    async (data: SignUpFormData) => {
+      try {
+        formRef.current?.setErrors({})
 
-      const schema = Yup.object().shape({
-        name: Yup.string().required('Nome obrigatório'),
-        email: Yup.string()
-          .required('E-mail obrigatório')
-          .email('Digite um e-mail válido'),
-        password: Yup.string().min(6, 'No mínimo 6 dígitos'),
-      })
+        const schema = Yup.object().shape({
+          name: Yup.string().required('Nome obrigatório'),
+          email: Yup.string()
+            .required('E-mail obrigatório')
+            .email('Digite um e-mail válido'),
+          password: Yup.string().min(6, 'No mínimo 6 dígitos'),
+        })
 
-      await schema.validate(data, {
-        abortEarly: false,
-      })
+        await schema.validate(data, {
+          abortEarly: false,
+        })
 
-      // await api.post('/users', data)
+        await api.post('/users', data)
 
-      // history.push('/')
-    } catch (err) {
-      if (err instanceof Yup.ValidationError) {
-        const errors = getValidationErrors(err)
-        formRef.current?.setErrors(errors)
+        Alert.alert(
+          'Cadastro realizado com sucesso!',
+          'Você já pode fazer login na aplicação.'
+        )
 
-        return
+        navigation.goBack()
+      } catch (err) {
+        if (err instanceof Yup.ValidationError) {
+          const errors = getValidationErrors(err)
+          formRef.current?.setErrors(errors)
+
+          return
+        }
+        Alert.alert(
+          'Erro no Cadastro',
+          'Ocorreu um erro ao realizar seu cadastro.'
+        )
       }
-      Alert.alert(
-        'Erro no Cadastro',
-        'Ocorreu um erro ao realizar seu cadastro.'
-      )
-    }
-  }, [])
-
-  const navigation = useNavigation()
+    },
+    [navigation]
+  )
 
   return (
     <>
