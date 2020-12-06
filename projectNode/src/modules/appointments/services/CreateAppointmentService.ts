ffa51b1c101633ec5916c4a7ab80d354e4dfbf41
Raynon Messias <raymessias@gmail.com>
2020-12-06 19:34:40 -0300

    projectnode: Apply SOLID principles

--- projectNode/src/modules/appointments/services/CreateAppointmentService.ts
+++ projectNode/src/modules/appointments/services/CreateAppointmentService.ts
@@ -4,25 +4,24 @@
  */
 
 import { startOfHour } from 'date-fns'
-import { getCustomRepository } from 'typeorm'
 
 import AppError from '@shared/errors/AppError'
 
+import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository'
 import Appointment from '../infra/typeorm/entities/Appointment'
-import AppointmentsRepository from '../repositories/AppointmentsRepository'
 
-interface Request {
+interface IRequest {
   date: Date
   provider_id: string
 }
 
 class CreateAppointmentService {
-  public async execute({ date, provider_id }: Request): Promise<Appointment> {
-    const appointmentsRepository = getCustomRepository(AppointmentsRepository)
+  constructor(private appointmentsRepository: IAppointmentsRepository) {}
 
+  public async execute({ date, provider_id }: IRequest): Promise<Appointment> {
     const appointmentDate = startOfHour(date)
 
-    const findAppointmentInSameDate = await appointmentsRepository.findByDate(
+    const findAppointmentInSameDate = await this.appointmentsRepository.findByDate(
       appointmentDate,
     )
 
@@ -30,12 +29,10 @@ class CreateAppointmentService {
       throw new AppError('This appointment is already booked')
     }
 
-    const appointment = appointmentsRepository.create({
+    const appointment = await this.appointmentsRepository.create({
       provider_id,
       date: appointmentDate,
     })
-
-    await appointmentsRepository.save(appointment)
 
     return appointment
   }
