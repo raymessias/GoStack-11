97b063a4280615d55eb7bfb8fde381fd3675a56f
Raynon Messias <raymessias@gmail.com>
2020-12-10 20:35:41 -0300

    projectnode: Apply model of email to reset password feature

--- projectNode/src/modules/users/services/SendForgotPasswordEmailService.ts
+++ projectNode/src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -1,5 +1,7 @@
 import { inject, injectable } from 'tsyringe'
 
+import path from 'path'
+
 import AppError from '@shared/errors/AppError'
 
 import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider'
@@ -32,6 +34,13 @@ class SendForgotPassEmailService {
 
     const { token } = await this.userTokensRepository.generate(user.id)
 
+    const forgotPasswordTemplate = path.resolve(
+      __dirname,
+      '..',
+      'views',
+      'forgot_password.hbs',
+    )
+
     await this.mailProvider.sendMail({
       to: {
         name: user.name,
@@ -39,10 +48,11 @@ class SendForgotPassEmailService {
       },
       subject: '[GoBarber] Recuperação de senha',
       templateData: {
-        template: 'Olá, {{name}}: {{token}}',
+        file: forgotPasswordTemplate,
         variables: {
           name: user.name,
           token,
+          link: `http://localhost:3000/reset_password?token=${token}`,
         },
       },
     })
--- projectNode/src/shared/container/providers/MailTemplateProvider/fakes/FakeMailTemplateProvider.ts
+++ projectNode/src/shared/container/providers/MailTemplateProvider/fakes/FakeMailTemplateProvider.ts
@@ -1,9 +1,8 @@
-import IParseMailTemplateDTO from '../dtos/IParseMailTemplateDTO'
 import IMailtemplateProvider from '../models/IMailTemplateProvider'
 
 class FakeMailTemplateProvider implements IMailtemplateProvider {
-  public async parse({ template }: IParseMailTemplateDTO): Promise<string> {
-    return template
+  public async parse(): Promise<string> {
+    return 'Mail content'
   }
 }
 
