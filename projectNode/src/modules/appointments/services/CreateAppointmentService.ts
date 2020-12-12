2bbd0ce35c9e43becf258562e19398d178e657d4
Raynon Messias <raymessias@gmail.com>
2020-12-11 23:54:16 -0300

    projectnode: Validate all test to create appointment feature

--- projectNode/src/modules/appointments/services/CreateAppointmentService.ts
+++ projectNode/src/modules/appointments/services/CreateAppointmentService.ts
@@ -3,7 +3,7 @@
  * Uma funcionalidade por arquivo, nesse só se cria um appointment
  */
 
-import { startOfHour } from 'date-fns'
+import { startOfHour, isBefore, getHours } from 'date-fns'
 import { inject, injectable } from 'tsyringe'
 import AppError from '@shared/errors/AppError'
 
@@ -29,6 +29,18 @@ class CreateAppointmentService {
     provider_id,
   }: IRequest): Promise<Appointment> {
     const appointmentDate = startOfHour(date)
+
+    if (user_id === provider_id) {
+      throw new AppError("You can't create an appointment to yourself")
+    }
+
+    if (isBefore(appointmentDate, Date.now())) {
+      throw new AppError("You can't create an appointment on a past date")
+    }
+
+    if (getHours(appointmentDate) < 8 || getHours(appointmentDate) > 17) {
+      throw new AppError('You can create appointments between 8am and 5pm')
+    }
 
     const findAppointmentInSameDate = await this.appointmentsRepository.findByDate(
       appointmentDate,
