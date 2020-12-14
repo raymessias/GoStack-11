ae7b06a2581511ae2044a95f8dfbb8ccb8325f84
Raynon Messias <raymessias@gmail.com>
2020-12-14 00:29:50 -0300

    projectnode: Create notification repository with mongodb

---
+++ projectNode/src/modules/notifications/infra/typeorm/repositories/NotificationsRepository.ts
@@ -0,0 +1,34 @@
+/**
+ * MongoRepository - Responsável por fazer as operações dos dados da aplicação,
+ */
+
+import { getMongoRepository, MongoRepository } from 'typeorm'
+
+import INotificationsRepository from '@modules/notifications/repositories/INotificationsRepository'
+import ICreateNotificationDTO from '@modules/notifications/dtos/ICreateNotificationDTO'
+
+import Notification from '../schemas/Notification'
+
+class NotificationsRepository implements INotificationsRepository {
+  private ormRepository: MongoRepository<Notification>
+
+  constructor() {
+    this.ormRepository = getMongoRepository(Notification, 'mongo')
+  }
+
+  public async create({
+    content,
+    recipient_id,
+  }: ICreateNotificationDTO): Promise<Notification> {
+    const notification = this.ormRepository.create({
+      content,
+      recipient_id,
+    })
+
+    await this.ormRepository.save(notification)
+
+    return notification
+  }
+}
+
+export default NotificationsRepository
