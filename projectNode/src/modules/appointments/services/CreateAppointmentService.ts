f0e69165316d83b3ad63b8ad7b567613bb223153
Raynon Messias <raymessias@gmail.com>
2020-12-14 22:38:45 -0300

    projectnode: Create test to notification feature

--- projectNode/src/modules/appointments/services/CreateAppointmentService.ts
+++ projectNode/src/modules/appointments/services/CreateAppointmentService.ts
@@ -3,11 +3,13 @@
  * Uma funcionalidade por arquivo, nesse só se cria um appointment
  */
 
-import { startOfHour, isBefore, getHours } from 'date-fns'
+import { startOfHour, isBefore, getHours, format } from 'date-fns'
 import { inject, injectable } from 'tsyringe'
 import AppError from '@shared/errors/AppError'
 
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository'
+import INotificationsRepository from '@modules/notifications/repositories/INotificationsRepository'
+
 import Appointment from '../infra/typeorm/entities/Appointment'
 
 interface IRequest {
@@ -21,6 +23,9 @@ class CreateAppointmentService {
   constructor(
     @inject('AppointmentsRepository')
     private appointmentsRepository: IAppointmentsRepository,
+
+    @inject('NotificationsRepository')
+    private notificationsRepository: INotificationsRepository,
   ) {}
 
   public async execute({
@@ -54,6 +59,13 @@ class CreateAppointmentService {
       user_id,
       provider_id,
       date: appointmentDate,
+    })
+
+    const dateFormatted = format(appointmentDate, "dd/MM/yyyy 'às' HH:mm'h'")
+
+    await this.notificationsRepository.create({
+      recipient_id: provider_id,
+      content: `Novo agendamento para dia ${dateFormatted}`,
     })
 
     return appointment
