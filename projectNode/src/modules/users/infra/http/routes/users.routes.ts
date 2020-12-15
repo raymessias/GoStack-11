0d86afb5726b924d0c186bbdf3ffdb2933f3d77b
Raynon Messias <raymessias@gmail.com>
2020-12-14 23:43:47 -0300

    projectnode: Create validation to routes

--- projectNode/src/modules/users/infra/http/routes/users.routes.ts
+++ projectNode/src/modules/users/infra/http/routes/users.routes.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express'
 import multer from 'multer'
+import { celebrate, Segments, Joi } from 'celebrate'
+
 import UploadConfig from '@config/upload'
+import ensureAuthenticated from '../middlewares/ensureAuthenticated'
 
 import UsersController from '../controllers/UsersController'
 import UserAvatarController from '../controllers/UserAvatarController'
-
-import ensureAuthenticated from '../middlewares/ensureAuthenticated'
 
 const usersRouter = Router()
 const upload = multer(UploadConfig)
@@ -13,7 +14,17 @@ const upload = multer(UploadConfig)
 const usersController = new UsersController()
 const userAvatarController = new UserAvatarController()
 
-usersRouter.post('/', usersController.create)
+usersRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      email: Joi.string().email().required(),
+      password: Joi.string().required(),
+    },
+  }),
+  usersController.create,
+)
 
 usersRouter.patch(
   '/avatar',
--- projectNode/src/shared/infra/http/server.ts
+++ projectNode/src/shared/infra/http/server.ts
@@ -2,6 +2,7 @@ import 'reflect-metadata'
 
 import express, { Request, Response, NextFunction } from 'express'
 import cors from 'cors'
+import { errors } from 'celebrate'
 import 'express-async-errors'
 
 import uploadConfig from '@config/upload'
@@ -18,6 +19,7 @@ app.use(express.json())
 app.use('/files', express.static(uploadConfig.uploadsFolder))
 app.use(routes)
 
+app.use(errors())
 // Exception global
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
   if (err instanceof AppError) {
