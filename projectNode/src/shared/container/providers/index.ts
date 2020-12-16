a69cc70b6acb3e7005416954a9fb1d0c12f9bab5
Raynon Messias <raymessias@gmail.com>
2020-12-16 01:15:51 -0300

    projectnode: Implements simple emal send with AWS SES

--- projectNode/src/shared/container/providers/index.ts
+++ projectNode/src/shared/container/providers/index.ts
@@ -1,25 +1,3 @@
-import { container } from 'tsyringe'
-
-import IStorageProvider from './StorageProvider/models/IStorageProvider'
-import DiskStoreProvider from './StorageProvider/implementations/DiskStorageProvider'
-
-import IMailProvider from './MailProvider/models/IMailProvider'
-import EtherealMailProvider from './MailProvider/implementations/EtherealMailProvider'
-
-import IMailTemplateProvider from './MailTemplateProvider/models/IMailTemplateProvider'
-import HandlebarsMailTemplateProvider from './MailTemplateProvider/implementations/HandlebarsMailTemplateProvider'
-
-container.registerSingleton<IStorageProvider>(
-  'StorageProvider',
-  DiskStoreProvider,
-)
-
-container.registerSingleton<IMailTemplateProvider>(
-  'MailTemplateProvider',
-  HandlebarsMailTemplateProvider,
-)
-
-container.registerInstance<IMailProvider>(
-  'MailProvider',
-  container.resolve(EtherealMailProvider),
-)
+import './StorageProvider'
+import './MailTemplateProvider'
+import './MailProvider'
