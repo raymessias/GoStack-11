89ae328d893222deab1541b2541eda3e70fb146f
Raynon Messias <raymessias@gmail.com>
2021-01-06 22:01:54 -0300

    projectnode: Create limiter request feature to app

--- projectNode/src/shared/infra/http/server.ts
+++ projectNode/src/shared/infra/http/server.ts
@@ -8,6 +8,7 @@ import 'express-async-errors'
 
 import uploadConfig from '@config/upload'
 import AppError from '@shared/errors/AppError'
+import rateLimiter from '@shared/infra/http/middlewares/rateLimiter'
 import routes from './routes'
 
 import '@shared/infra/typeorm'
@@ -15,12 +16,14 @@ import '@shared/container'
 
 const app = express()
 
+app.use(rateLimiter)
 app.use(cors())
 app.use(express.json())
 app.use('/files', express.static(uploadConfig.uploadsFolder))
 app.use(routes)
 
 app.use(errors())
+
 // Exception global
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
   if (err instanceof AppError) {
